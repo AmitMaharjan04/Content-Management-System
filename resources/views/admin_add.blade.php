da6133aeedb3cf3f3817db9cd282f5a165e50e7a
Amit <amit@gmail.com>
2023-04-19 16:53:18 +0545

    test

--- resources/views/admin_add.blade.php
+++ resources/views/admin_add.blade.php
@@ -221,7 +221,7 @@
         {{ Form::close() }}
     </main>
     <footer>
-        <!-- place footer here -->
+        <!-- place footer here ---->
     </footer>
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
